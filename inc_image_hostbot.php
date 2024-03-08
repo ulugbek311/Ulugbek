96e2cfb7c126bcfd3d0a3dbd995cd5cb5db9b874
ulugbek311 <162639227+ulugbek311@users.noreply.github.com>
2024-03-08 22:34:29 +0500

    update inc_image_hostbot.php

---
+++ inc_image_hostbot.php
@@ -0,0 +1,8 @@
+////////////Bu .jpg .png va shunga uxshash filelarni hostga yuklaydi --- @Coder_Bro dan @PHP_Bot_Kodlari uchun
+if(isset($message->photo)){
+    $Photo = $message->photo;
+    $Photo_File_id = $Photo[count($Photo) - 1]->file_id;
+    $Photo_Path = bot('getfile', ['file_id' => $Photo_File_id])->result->file_path;
+    $Photo_extension = pathinfo($Photo_Path, PATHINFO_EXTENSION);
+        file_put_contents('Photo.'.$Photo_extension , file_get_contents('https://api.telegram.org/file/bot' . API_KEY . '/' . $Photo_Path));
+        }
